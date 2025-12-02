c6c9b5daab09dcce074a6fb3fdeed7cdff4553b6
BryanElChino <b2104gm@gmail.com>
2025-12-01 20:22:23 -0600

    BibliotecaFinal.py

--- Biblioteca.py
+++ Biblioteca.py
@@ -16,15 +16,15 @@ class Libro:
     
     def getCodigo(self):
         return self.__codigo
-    
+        
     def mostrarAtrib(self):
         print("Título:", self.__titulo)
         print("Código:", self.__codigo)
         print("Autor:", self.autor)
         print("Editorial:", self.editorial)
 
-    def verificarAutor(self, autoBuscado):
-        if autoBuscado.lower() in self.autor.lower():
+    def verificarAutor(self, autorBuscado):
+        if autorBuscado.lower() in self.autor.lower():
             return True
         else:
             return False
@@ -62,8 +62,8 @@ class Prestamo:
         print("Fecha Devolución:", self.getFechaDevolucion())
 
     def validarFecha(self, fecha_texto):
-        n = len(fecha_texto)
-        if n != 10: return False
+        if len(fecha_texto) != 10: return False
+        
         if fecha_texto[2] != "/" or fecha_texto[5] != "/": return False
         
         dia_txt = fecha_texto[0:2]
@@ -73,14 +73,31 @@ class Prestamo:
         if not (dia_txt.isdigit() and mes_txt.isdigit() and anio_txt.isdigit()):
             return False
         
-        dia = int(dia_txt)
-        mes = int(mes_txt)
-        anio = int(anio_txt)
+        d = int(dia_txt)
+        m = int(mes_txt)
+        a = int(anio_txt)
 
-        if dia < 1 or dia > 31: return False
-        if mes < 1 or mes > 12: return False
-        if anio < 2000 or anio > 2030: return False
+        if a < 2025 or a > 2030: return False
+        if m < 1 or m > 12: return False
+
+        dias_limite = 31 # Por defecto la mayoría tiene 31
         
+        # Meses con 30 días: Abril(4), Junio(6), Septiembre(9), Noviembre(11)
+        if m == 4 or m == 6 or m == 9 or m == 11:
+            dias_limite = 30
+            
+        # Febrero (2)
+        elif m == 2:
+            # Regla simple de bisiesto: divisible entre 4
+            if a % 4 == 0:
+                dias_limite = 29
+            else:
+                dias_limite = 28
+        
+        # Verificamos si el día se pasa del límite de ese mes
+        if d < 1 or d > dias_limite:
+            return False
+
         return True
     
     def corregirFechaPrestamo(self):
@@ -108,15 +125,15 @@ total_libros = 0
 total_prestamos = 0
 
 librero[0] = Libro("El Principito", 101, "Antoine de Saint-Exupery", "Salamandra")
-total_libros += 1
+total_libros = total_libros + 1
 librero[1] = Libro("Cien anios de soledad", 102, "Gabriel Garcia Marquez", "Diana")
-total_libros += 1
+total_libros = total_libros + 1
 librero[2] = Libro("Don Quijote", 103, "Miguel de Cervantes", "Alfaguara")
-total_libros += 1
+total_libros = total_libros + 1
 librero[3] = Libro("Harry Potter", 104, "J.K. Rowling", "Salamandra")
-total_libros += 1
+total_libros = total_libros + 1
 librero[4] = Libro("Steve Jobs", 105, "Walter Isaacson", "Debate")
-total_libros += 1
+total_libros = total_libros + 1
 
 print("Sistema iniciado ", total_libros, "libros cargados correctamente.")
 
@@ -136,14 +153,14 @@ for k in range(n):
 
     print("Introduce el código numérico:")
     new_cod = int(input())
-    repetido = False
 
-    # Validación de código único
+     # Validación de código único
+    repetido = False
     for i in range(total_libros):
         if librero[i].getCodigo() == new_cod:
             repetido = True
 
-    if repetido:
+    if repetido==True:
         print("¡ERROR! Ese código ya existe. No se guardará.")
     else:
         print("Introduce el Título:")
@@ -155,8 +172,8 @@ for k in range(n):
 
         nuevo_libro = Libro(tit, new_cod, aut, edi)
         librero[total_libros] = nuevo_libro
-        total_libros += 1
-        print("Libro guardado.")
+        total_libros =total_libros + 1
+        print("Libro guardado...")
 
 
 # ==========================================
@@ -165,36 +182,37 @@ for k in range(n):
 
 opcion = ""
 
-while opcion != "i":
-    print("\n--- GESTIÓN DE BIBLIOTECA ---")
-    print("a) Mostrar libros")
-    print("b) Buscar libro por autor")
-    print("c) Dar de baja un libro")
-    print("d) Registrar préstamo")
-    print("e) Registrar devolución")
-    print("f) Mostrar historial de préstamos")
-    print("g) Ver libros DISPONIBLES (Extra)")
-    print("h) Corregir fecha de préstamo")
-    print("i) Salir")
+while opcion != "10":
+    print("--- GESTIÓN DE BIBLIOTECA ---")
+    print("1. Mostrar libros")
+    print("2. Buscar libro por autor")
+    print("3. Dar de baja un libro")
+    print("4. Registrar préstamo")
+    print("5. Registrar devolución")
+    print("6. Mostrar historial de préstamos")
+    print("7. Ver libros DISPONIBLES (Extra)")
+    print("8. Corregir fecha de préstamo")
+    print("9. Agregar un libro nuevo (extra)" ) 
+    print("10. Salir")
     
-    opcion = input("Elige una opción: ").lower()
+    opcion = input("Elige una opción (número): ")
 
-    opcionValida=False
+    opcionValida = False
 
-    # a) MOSTRAR LIBROS
-    if opcion == "a":
-        opcionValida=True
+    # 1. MOSTRAR LIBROS
+    if opcion == "1":
+        opcionValida = True
         if total_libros == 0:
             print("Librero vacío.")
         else:
-            print("LISTADO",total_libros, "libros")
+            print("LISTADO DE", total_libros, "LIBROS")
             for i in range(total_libros):
                 librero[i].mostrarAtrib()
                 print("-------------------------")
 
-    # b) BUSCAR POR AUTOR
-    if opcion == "b":
-        opcionValida=True
+    # 2. BUSCAR POR AUTOR
+    if opcion == "2":
+        opcionValida = True
         print("Introduce el nombre (o parte) del autor:")
         busqueda = input()
         encontrado = False
@@ -203,12 +221,12 @@ while opcion != "i":
                 librero[i].mostrarAtrib()
                 print("-------------------------")
                 encontrado = True
-        if not encontrado:
+        if encontrado == False:
             print("No se encontraron coincidencias.")
 
-    # c) DAR DE BAJA UN LIBRO
-    if opcion == "c":
-        opcionValida=True
+    # 3. DAR DE BAJA
+    if opcion == "3":
+        opcionValida = True
         print("Introduce el CÓDIGO del libro a borrar:")
         cod_borrar = int(input())
         posicion = -1
@@ -218,18 +236,18 @@ while opcion != "i":
                 posicion = i
         
         if posicion != -1:
-            print(f"Eliminando: {librero[posicion].getTitulo()}...")
+            print("Eliminando:", librero[posicion].getTitulo())
             for j in range(posicion, total_libros - 1):
                 librero[j] = librero[j+1]
             librero[total_libros - 1] = None
-            total_libros -= 1
+            total_libros = total_libros - 1
             print("Libro eliminado.")
         else:
             print("Error: Código no encontrado.")
 
-    # d) REGISTRAR PRÉSTAMO
-    if opcion == "d":
-        opcionValida=True
+    # 4. REGISTRAR PRÉSTAMO
+    if opcion == "4":
+        opcionValida = True
         if total_prestamos >= MAX_CAPACIDAD:
             print("Error: Memoria llena.")
         else:
@@ -242,7 +260,7 @@ while opcion != "i":
                     existe = True
                     print("Libro:", librero[i].getTitulo())
             
-            if existe:
+            if existe == True:
                 print("Nombre del alumno:")
                 alumno = input()
                 print("Fecha (dd/mm/aaaa):")
@@ -252,16 +270,16 @@ while opcion != "i":
                 if temp.validarFecha(fecha):
                     nuevo_p = Prestamo(cod, alumno, fecha)
                     prestamos[total_prestamos] = nuevo_p
-                    total_prestamos += 1
+                    total_prestamos = total_prestamos + 1
                     print("Préstamo registrado.")
                 else:
                     print("Fecha inválida.")
             else:
                 print("Libro no encontrado.")
 
-    # e) DEVOLUCIÓN
-    if opcion == "e":
-        opcionValida=True
+    # 5. DEVOLUCIÓN
+    if opcion == "5":
+        opcionValida = True
         print("Código del libro devuelto:")
         cod = int(input())
         encontrado = False
@@ -269,7 +287,7 @@ while opcion != "i":
         for i in range(total_prestamos):
             p_actual = prestamos[i]
             if p_actual.getCodPedido() == cod and p_actual.getFechaDevolucion() == "Pendiente":
-                print(f"Alumno: {p_actual.getNombAlumno()}")
+                print("Alumno:", p_actual.getNombAlumno())
                 print("Fecha devolución (dd/mm/aaaa):")
                 f_dev = input()
                 
@@ -280,23 +298,23 @@ while opcion != "i":
                 else:
                     print("Fecha inválida.")
         
-        if not encontrado:
+        if encontrado == False:
             print("No hay préstamo pendiente para ese libro.")
 
-    # f) HISTORIAL
-    if opcion == "f":
-        opcionValida=True
+    # 6. HISTORIAL
+    if opcion == "6":
+        opcionValida = True
         if total_prestamos == 0:
             print("No hay historial.")
         else:
-            print("--- HISTORIAL ", total_prestamos, "registros ---")
+            print("--- HISTORIAL", total_prestamos, "REGISTROS ---")
             for i in range(total_prestamos):
                 prestamos[i].mostrarObj()
                 print("-----------------")
 
-    # g) LIBROS DISPONIBLES (metodo extra)
-    if opcion == "g":
-        opcionValida=True
+    # 7. LIBROS DISPONIBLES
+    if opcion == "7":
+        opcionValida = True
         print("--- LIBROS DISPONIBLES ---")
         for i in range(total_libros):
             cod_actual = librero[i].getCodigo()
@@ -304,30 +322,66 @@ while opcion != "i":
             for k in range(total_prestamos):
                 if prestamos[k].getCodPedido() == cod_actual and prestamos[k].getFechaDevolucion() == "Pendiente":
                     esta_prestado = True
-            if not esta_prestado:
-                print(librero[i].getTitulo(),"Cod: ", cod_actual)
 
-    #CORREGIR FECHA DEL PRESTAMO
-    if opcion=="h":
+            if esta_prestado == False:
+                print(librero[i].getTitulo(), "Cod:", cod_actual)
+
+    # 8. CORREGIR FECHA
+    if opcion == "8":
+        opcionValida = True
         print("Ingrese el CÓDIGO del libro del préstamo a corregir:")
         cod = int(input())
         encontrado = False
 
         for i in range(total_prestamos):
             if prestamos[i].getCodPedido() == cod and prestamos[i].getFechaDevolucion() == "Pendiente":
-                print("Préstamo encontrado. Alumno: ", prestamos[i].getNombAlumno())
-                print("Fecha actual registrada: ",prestamos[i].fecha)
-
+                print("Préstamo encontrado. Alumno:", prestamos[i].getNombAlumno())
+                print("Fecha actual registrada:", prestamos[i].fecha)
                 prestamos[i].corregirFechaPrestamo()
-                
                 encontrado = True
         
-        if not encontrado:
+        if encontrado == False:
             print("No se encontró un préstamo activo con ese código.")
-    #SALIR DEL PROGRAMA
-    if opcion == "i":
-        opcionValida=True
-        print("Saliend  o...")
+            
+    # 9. AGREGAR NUEVO LIBRO
+    if opcion == "9":
+        opcionValida = True
+        if total_libros >= MAX_CAPACIDAD:
+            print("Error: El librero está lleno.")
+        else:
+            print("--- ALTA DE LIBRO ---")
+            print("Introduce el código numérico:")
+            cod_str = input()
+            
+            if cod_str.isdigit(): 
+                new_cod = int(cod_str)
+                repetido = False
+                
+                for i in range(total_libros):
+                    if librero[i].getCodigo() == new_cod:
+                        repetido = True
+                
+                if repetido == True:
+                    print("¡ERROR! Ya existe un libro con ese código.")
+                else:
+                    print("Introduce el Título:")
+                    tit = input()
+                    print("Introduce el Autor:")
+                    aut = input()
+                    print("Introduce la Editorial:")
+                    edi = input()
+
+                    nuevo_libro = Libro(tit, new_cod, aut, edi)
+                    librero[total_libros] = nuevo_libro
+                    total_libros = total_libros + 1
+                    print("Libro agregado exitosamente.")
+            else:
+                print("Error: El código debe ser un número.")
+
+    # 10. SALIR
+    if opcion == "10":
+        opcionValida = True
+        print("Saliendo...")
     
-    if opcionValida== False:
-        print("Opcion no valida, intente de nuevo ")
+    if opcionValida == False:
+        print("Opción no válida, intente de nuevo con un número del 1 al 10.")
